0c37557670fe34482045bd2175507bb49a2099eb
James Harris <mharris9608@yahoo.com>
2021-02-22 18:15:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,6 @@
-
 class CashRegister
 
-  attr_accessor :total, :discount, :last_transaction_amount, :items
+  attr_accessor :total, :discount, :items, :last_item_price
 
   def initialize(discount = 0)
     @total = 0
@@ -9,35 +8,29 @@ class CashRegister
     @items = []
   end
 
-
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    @last_item_price = price * quantity
+    while quantity.positive?
       @items << title
+      quantity -= 1
     end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
   end
 
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
+  def apply_discount
+    if self.discount > 0
+      self.total -= ((self.discount/100.0)*self.total)
+      success_message = "After the discount, the total comes to $#{self.total.to_i}."
+      success_message
     else
-      "There is no discount to apply."
+      failure_message = "There is no discount to apply."
+      failure_message
     end
   end
 
-
-  def void_last_transaction()
-    @total = (@total - @last_transaction_amount)
+  def void_last_transaction
+    self.total -= @last_item_price
+    self.total
   end
 
 end
